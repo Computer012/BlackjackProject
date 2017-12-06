6d5300cdaac7e4f42ed00007f144f0c736396ed0
Park sangmin <smpsmp0509@gmail.com>
2017-12-07 00:37:52 +0900

    create ClientBackground with Socket, Thread, Player(Gamer) code.

--- src/com/sangmin/blackjack/Control/ClientBackground.java
+++ src/com/sangmin/blackjack/Control/ClientBackground.java
@@ -1,31 +1,33 @@
 package com.sangmin.blackjack.Control;
 
-import java.awt.*;
-import java.awt.event.*;
 import java.io.*;
 import java.net.*;
 import java.util.*;
-import javax.swing.*;
+import java.util.List;
 
 import com.sangmin.blackjack.Construction.Card;
+import com.sangmin.blackjack.Construction.Player;
 import com.sangmin.blackjack.GUI.GameWaitingView;
 
-public class ClientBackground {
+public class ClientBackground implements Player {
 
+	public static void main(String[] args) {
+		new ClientBackground("computer012");
+	}
+
+	/**************** Client ****************/
+	// 통신 관련 Data
 	private Socket socket = null;
 	private ObjectOutputStream oos = null;
 	private Receiver receiver = null;
 	private Thread thread = null;
-	private String id = null;
-	
 	private GameWaitingView waitingView = null;
-	private LinkedList<Card> cardList = null;
 
+	// Client 생성자 method
 	public ClientBackground(String id) {
-		
-		this.id = id;
+		setName(id);
 		waitingView = new GameWaitingView();
-		
+
 		try {
 			// Server의 IP, Port number로 받아와 소켓 생성
 			socket = new Socket("127.0.0.1", 9000);
@@ -42,37 +44,86 @@ public class ClientBackground {
 		}
 
 	}
+
+	/**************** Game ****************/
+	// Game 관련 Data
+	private List<Card> cards = new ArrayList<>();
+	private boolean turn;
+	private String name;
+
+	// Card 받는 method
+	@Override
+	public void receiveCard(Card card) {
+		this.cards.add(card);
+		this.showCards();
+		waitingView.addCard(card.toString());
+	}
+
+	// Gamer가 현재 가지고 있는 카드 목록을 보여주는 method
+	@Override
+	public void showCards() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("현재 보유 카드 목록 \n");
+
+		for (Card card : cards) {
+			sb.append(card.toString());
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+
+	// Gamer가 현재 가지고 있는 카드 목록을 반환하는 method
+	@Override
+	public List<Card> openCards() {
+		return this.cards;
+	}
+
+	// turn을 off로 요청하는 method
+	@Override
+	public void turnOff() {
+		this.setTurn(false);
+	}
+
+	// turn을 on으로 요청하는 method
+	@Override
+	public void turnOn() {
+		
+		this.setTurn(true);
+	}
+
+	// turn의 값을 반환하는 method
+	@Override
+	public boolean isTurn() {
+		return this.turn;
+	}
+
+	// turn을 setting하는 method
+	private void setTurn(boolean turn) {
+		this.turn = turn;
+	}
+
+	// name을 setting하는 method
+	public void setName(String name) {
+		this.name = name;
+	}
 	
-	public void addCard(Card card) {
-		cardList.add(card);
+	// name을 반환하는 method
+	@Override
+	public String getName() {
+		return this.name;
 	}
 
-	// content 객체를 서버로 전송하는 method
-//	public void send(String string) {
-//		try {
-//			oos.writeObject();
-//			oos.flush();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
-
-	public static void main(String[] args) {
-		new ClientBackground("idTest");
-	}
-
+	
 	// 수신을 담당하는 Client측 Thread
 	class Receiver implements Runnable {
 		private ObjectInputStream ois = null;
-		private String time = null;
 		volatile boolean connect = false;
 
 		public Receiver(Socket socket) {
-			
+
 			try {
 				ois = new ObjectInputStream(socket.getInputStream());
 				connect = true;
-
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -81,19 +132,29 @@ public class ClientBackground {
 		public boolean isConnected() {
 			return connect;
 		}
-		
+
 		public void setConnectToDisconnect() {
 			connect = false;
 		}
 
 		@Override
 		public void run() {
-			
+
+			try {
+				System.out.println("Receiver 동작, " + name + " 전송");
+				oos.writeObject(name); // id 전송하기
+				setName(name);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+
 			// 연결이 끊길 때 까지 반복
+			System.out.println("연결 끊길때 까지 반복 예정");
 			while (connect) {
 				try {
-					time = (String) ois.readObject();
-					waitingView.sets(time);
+					Card card = (Card) ois.readObject();
+					System.out.println("카드 한장 받음!");
+					receiveCard(card);
 				} catch (ClassNotFoundException e) {
 					e.printStackTrace();
 					break;
@@ -107,4 +168,3 @@ public class ClientBackground {
 		}
 	}
 }
-
