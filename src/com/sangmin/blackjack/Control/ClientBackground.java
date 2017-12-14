c48616966b9ed2158af6b16cfe6b497b508d0ce9
Park sangmin <smpsmp0509@gmail.com>
2017-12-14 16:06:38 +0900

    초기화로 Dealer card 2장, Gamer card 2장을 받는 것, 그리고 oos로 추가 카드를 요청하는 메소드까지 구현

--- src/com/sangmin/blackjack/Control/ClientBackground.java
+++ src/com/sangmin/blackjack/Control/ClientBackground.java
@@ -7,12 +7,13 @@ import java.util.List;
 
 import com.sangmin.blackjack.Construction.Card;
 import com.sangmin.blackjack.Construction.Player;
-import com.sangmin.blackjack.GUI.GameWaitingView;
+import com.sangmin.blackjack.Construction.SendingType;
+import com.sangmin.blackjack.GUI.GameView;
 
 public class ClientBackground implements Player {
 
 	public static void main(String[] args) {
-		new ClientBackground("computer012");
+		new ClientBackground("computer");
 	}
 
 	/**************** Client ****************/
@@ -21,12 +22,12 @@ public class ClientBackground implements Player {
 	private ObjectOutputStream oos = null;
 	private Receiver receiver = null;
 	private Thread thread = null;
-	private GameWaitingView waitingView = null;
+	private GameView waitingView = null;
 
 	// Client 생성자 method
 	public ClientBackground(String id) {
 		setName(id);
-		waitingView = new GameWaitingView();
+		waitingView = new GameView(this);
 
 		try {
 			// Server의 IP, Port number로 받아와 소켓 생성
@@ -47,10 +48,18 @@ public class ClientBackground implements Player {
 
 	/**************** Game ****************/
 	// Game 관련 Data
+	private static final int INIT_RECEIVE_CARD_COUNT = 2;
+	private List<Card> dealerCards = new ArrayList<>();
 	private List<Card> cards = new ArrayList<>();
 	private boolean turn;
 	private String name;
 
+	// Dealer의 Card를 받는 method
+	public void dealerCardReceive(Card card) {
+		this.dealerCards.add(card);
+		waitingView.addDealerCard(card.toString());
+	}
+	
 	// Card 받는 method
 	@Override
 	public void receiveCard(Card card) {
@@ -71,13 +80,19 @@ public class ClientBackground implements Player {
 		}
 		System.out.println(sb.toString());
 	}
-
-	// Gamer가 현재 가지고 있는 카드 목록을 반환하는 method
-	@Override
-	public List<Card> openCards() {
-		return this.cards;
+	
+	// Server측에 카드를 더 받을지 여부를 전송
+	public void cardRequest() {
+		try {
+			oos.writeObject(new SendingType(name, turn));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("까꿍 카드 리퀘스트~");
 	}
-
+	
+	
+	
 	// turn을 off로 요청하는 method
 	@Override
 	public void turnOff() {
@@ -87,18 +102,17 @@ public class ClientBackground implements Player {
 	// turn을 on으로 요청하는 method
 	@Override
 	public void turnOn() {
-		
 		this.setTurn(true);
 	}
 
 	// turn의 값을 반환하는 method
-	@Override
+ 	@Override
 	public boolean isTurn() {
 		return this.turn;
 	}
 
 	// turn을 setting하는 method
-	private void setTurn(boolean turn) {
+	public void setTurn(boolean turn) {
 		this.turn = turn;
 	}
 
@@ -113,6 +127,20 @@ public class ClientBackground implements Player {
 		return this.name;
 	}
 
+	// Gamer가 현재 가지고 있는 카드 목록을 반환하는 method
+	@Override
+	public List<Card> openCards() {
+		return this.cards;
+	}
+
+	public int getPointSum() {
+		int sum = 0;
+		
+		for(Card card : cards) {
+			sum += card.getPoint();
+		}
+		return sum;
+	}
 	
 	// 수신을 담당하는 Client측 Thread
 	class Receiver implements Runnable {
@@ -144,26 +172,37 @@ public class ClientBackground implements Player {
 				System.out.println("Receiver 동작, " + name + " 전송");
 				oos.writeObject(name); // id 전송하기
 				setName(name);
+				
+				for (int i=0; i<INIT_RECEIVE_CARD_COUNT; i++) {
+					Card card = (Card) ois.readObject();
+					System.out.println("딜러카드 한장 !");
+					dealerCardReceive(card);
+				}
+			
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
-
+			
 			// 연결이 끊길 때 까지 반복
 			System.out.println("연결 끊길때 까지 반복 예정");
-			while (connect) {
+			turnOn();
+			while (isConnected() && isTurn()) {
 				try {
 					Card card = (Card) ois.readObject();
-					System.out.println("카드 한장 받음!");
+					System.out.println("플레이어 카드 한장 !");
 					receiveCard(card);
 				} catch (ClassNotFoundException e) {
 					e.printStackTrace();
+					setConnectToDisconnect();
 					break;
 				} catch (IOException e) {
 					e.printStackTrace();
+					setConnectToDisconnect();
 					break;
 				}
 			}
-			connect = false;
 			System.out.println("Connection closed...");
 		}
 	}
