3e00ac2c5618e0de1cc9422acdda7ef11b692b0a
Park sangmin <smpsmp0509@gmail.com>
2017-12-15 00:43:02 +0900

    add total score int field.

--- src/com/sangmin/blackjack/Construction/SendingType.java
+++ src/com/sangmin/blackjack/Construction/SendingType.java
@@ -5,18 +5,23 @@ import java.io.Serializable;
 public class SendingType implements Serializable {
 	private String id;
 	private boolean turn;
+	private int totalScore;
 	
-	public SendingType(String id, boolean turn) {
+	public SendingType(String id, boolean turn, int totalScore) {
 		this.id = id;
 		this.turn = turn;
+		this.totalScore = totalScore;
 	}
 
 	public String getId() {
 		return id;
 	}
 
+	public int getTotalScore() {
+		return totalScore;
+	}
+
 	public boolean isTurn() {
 		return turn;
 	}
-	
 }
