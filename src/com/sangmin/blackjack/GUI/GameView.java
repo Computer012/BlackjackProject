226b1667b281914b4421923270cfe46dabd5e587
Park sangmin <smpsmp0509@gmail.com>
2017-12-14 16:09:56 +0900

    Dealer와 Gamer카드 2장씩 이미지 출력,버튼 클릭 추가 카드 요청 메소드 호출, 추가 카드 이미지 출력까지 구현

--- src/com/sangmin/blackjack/GUI/GameView.java
+++ src/com/sangmin/blackjack/GUI/GameView.java
@@ -1,43 +1,94 @@
 package com.sangmin.blackjack.GUI;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Stack;
 
 import javax.swing.*;
 
+import com.sangmin.blackjack.Control.ClientBackground;
+
 public class GameView {
 
-	public static void main(String[] args) {
-		new GameView();
-	}
-
+//	public static void main(String[] args) {
+//		new GameView();
+//	}
+	
+	private Stack<String> dealerCards = new Stack<String>();
 	private Stack<String> cards = new Stack<String>();
 	private DrawPanel panel = null;
-	public GameView() {
+	
+	public GameView(ClientBackground client) {
 		JFrame frame = new JFrame("Waiting Room");
 		frame.setLocation(100, 30);
 		frame.setPreferredSize(new Dimension(1218, 847));
 		
 		Container contentPane = frame.getContentPane();
 		
+		JButton hitButton = new JButton("HIT"); // 카드 더 받기
+		hitButton.setFont(new Font("맑은 고딕", 1, 30));
+		hitButton.setBounds(250, 480, 150, 100);
+		hitButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// client측에서 turn을 true로 해서 server에다가 보내도록 요청하는 코드를 작성해야
+				if (client.getPointSum() >= 21) {
+					client.turnOff();
+					System.out.println("21이 넘었으므로 불가!");
+				}
+				client.cardRequest();
+			}
+		});
+		JButton stayButton = new JButton("STAY"); // 카드 그만 받기
+		stayButton.setFont(new Font("맑은 고딕", 1, 30));
+		stayButton.setBounds(250, 620, 150, 100);
+		stayButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// client측에서 turn을 false로 해서 server에다가 보내도록 요청하는 코드를 작성해야
+				client.turnOff();
+				client.cardRequest();
+				showDealerCard();
+			}
+		});
+		
 		panel = new DrawPanel();
+		panel.setLayout(null);
+		panel.add(hitButton);
+		panel.add(stayButton);
 		contentPane.add(panel);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
 	}
-	
+
 	public void addCard(String card) {
 		cards.add(card);
 		panel.repaint();
 	}
 	
+	public void addDealerCard(String card) {
+		dealerCards.add(card);
+		panel.repaint();
+	}
+	
+	private void showDealerCard() {
+		
+	}
+	
 	// 그려주는 Panel
 	@SuppressWarnings("serial")
 	class DrawPanel extends JPanel {
-		Image backGroundImg = null, cardImg = null, backCardImg = null;
+		Image backGroundImg = null;
+		Image cardImg = null;
+		Image backCardImg = null;
+		Image dealerCardImg = null;
 		Toolkit toolkit = getToolkit();
+		boolean gameEnd = false;
 
 		public DrawPanel() {
 			backGroundImg = toolkit.getImage("C:\\Users\\User\\Desktop\\java project\\image\\Card\\Play_Table.png");
@@ -50,15 +101,21 @@ public class GameView {
 			
 			if (backGroundImg != null)
 				g.drawImage(backGroundImg, 0, 0, 1200, 800, this);
+			if (backCardImg != null)
+				g.drawImage(backCardImg, 550, 50, 210, 300, this);
+			if (!dealerCards.isEmpty()) {
+				dealerCardImg = toolkit.getImage("C:\\Users\\User\\Desktop\\java project\\image\\Card\\" + dealerCards.get(0) + ".png");
+				if (dealerCardImg != null)
+					g.drawImage(dealerCardImg, 460, 50, 210, 300, this);
+				
+			}
 			
 			if (!cards.isEmpty()) {
 				int i = 0;
 				for (String card : cards) {
-					if (backCardImg != null)
-						g.drawImage(backCardImg, 395+(210)*i, 50, 200, 300, this);
 					cardImg = toolkit.getImage("C:\\Users\\User\\Desktop\\java project\\image\\Card\\" + card + ".png");
 					if (cardImg != null)
-						g.drawImage(cardImg, 395+(210)*i, 450, 200, 300, this);
+						g.drawImage(cardImg, 450+(90)*i, 450, 210, 300, this);
 					i++;
 				}
 			}
