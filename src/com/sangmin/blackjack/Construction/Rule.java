fcf20816ab3d564467127a6c56ea976b4b1f825f
Park sangmin <smpsmp0509@gmail.com>
2017-12-16 19:21:30 +0900

    Rule 알고리즘 최소화

--- src/com/sangmin/blackjack/Construction/Rule.java
+++ src/com/sangmin/blackjack/Construction/Rule.java
@@ -1,7 +1,6 @@
 package com.sangmin.blackjack.Construction;
 
 public class Rule {
-	
 	// 점수를 비교해 승자가 누군지 반환하는 method
 	public String getWinner(int playerScore, int dealerScore, String id) {
 		String winner = "";
